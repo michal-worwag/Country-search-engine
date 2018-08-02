702ba0f160744ca6b2ea4308e352ce64a1107f42
Michał Worwąg <michalwo@gmail.com>
2018-08-02 19:02:13 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+  }
+
