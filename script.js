f588af6e2996a15cf0d5befc6e18d5cf07dffb19
Michał Worwąg <michalwo@gmail.com>
2018-08-03 18:36:01 +0200

    Testing

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('search').addEventListener('click', searchCountries);
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
-    fetch(url + countryName)
+    fetch('https://restcountries.eu/rest/v2/name/{name}' + countryName)
         .then(function(resp) {
             return resp.json();
         })
