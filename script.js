43683492f34530986e51dc51575ea3a848043bdc
Michał Worwąg <michalwo@gmail.com>
2018-08-05 14:53:06 +0200

    Task finished

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ document.getElementById('search').addEventListener('click', searchCountries);
 
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
-    if(!countryName.length) {countryName = 'Poland';}
-    fetch('https://restcountries.eu/rest/v2/name/' + countryName)
-        .then(function(resp) {
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+    .then(function(resp) {
+        if (resp.ok){
             return resp.json();
-        })
-        .then(showCountriesList);
+        } else {
+            return Promise.reject(resp)
+        }
+    })
+    .then (showCountriesList)
+    .catch(function(error) {
+        if (error.status === 404) {
+            alert('Country not found');
+        }
+    });
 }
+
 
 function showCountriesList(resp) {
     countriesList.innerHTML = '';
+    var liEl = document.createElement('li');
     resp.forEach(function(item) {
         var liEl = document.createElement('li');
-        if (item.name == true) {
         liEl.innerText = item.name;
         countriesList.appendChild(liEl);
-        } else {
-            liEl.innerText = 'Country not found';
-        countriesList.appendChild(liEl);
-        }
     });
 }
